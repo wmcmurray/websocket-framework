de10d0436bd13ba9e5c3eac00d03f13c042f2434
William Mcmurray <tiwilinlove@hotmail.com>
2013-07-20 02:29:35 -0400

    missing file in last commit

---
+++ public/lib/EventsDispatcher.js
@@ -0,0 +1,28 @@
+//===========================================================
+// ===== EventsDispatcher() =================================
+//===========================================================
+
+function EventsDispatcher()
+{
+	this.events_listeners = new Array();
+}
+
+EventsDispatcher.prototype.on = function (evt, fnc)
+{
+    if(!this.events_listeners[evt])
+    {
+        this.events_listeners[evt] = new Array();
+    }
+    this.events_listeners[evt].push(fnc);
+}
+
+EventsDispatcher.prototype.trigger = function (evt, args)
+{
+    if(this.events_listeners[evt])
+    {
+        for(var i in this.events_listeners[evt])
+        {
+            this.events_listeners[evt][i](args ? args : "");
+        }
+    }
+}
