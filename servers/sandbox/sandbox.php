704d62941e2af0731da3e8e5aab10fd5c6490710
William Mcmurray <tiwilinlove@hotmail.com>
2013-07-27 02:29:54 -0400

    Remove unwanted files added in previous commit

--- servers/sandbox/sandbox.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("Sandbox_SocketServer.php");
-
-$server = new Sandbox_SocketServer($CONFIG["default_ip"], 8079);
-$server->set_server_name("Sandbox");
-$server->register_admin_command("destroy");
-$server->run();
-?>
